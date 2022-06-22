34f1081fe01b06a19326597e0cb1709b9b7689df
StrangeHum <vadim.k.2004@yandex.ru>
2022-06-22 18:33:19 +0300

    Добавьте файлы проекта.

---
+++ ML1_Ddass/Neuron.cs
@@ -0,0 +1,7 @@
+﻿namespace MachineLearning_ML._1_Ddass
+{
+    public class Neuron
+    {
+        public List<double> Weight { get; }
+    }
+}
---
+++ ML1_Ddass/NeyronType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineLearning_ML._1_Ddass
+{
+    internal class NeyronType
+    {
+    }
+}
